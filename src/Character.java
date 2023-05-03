10ebcd4160c903716b188bde177f4fe3f4b431e4
BT_4N2_15 <jxu5406@bths.edu>
2023-05-03 12:30:10 -0400

    Patch v0.02 Advanced Character.java to use and write to files Added all the attributes of a character to Character.java Added more functional methods to Character.java

--- src/Character.java
+++ src/Character.java
@@ -1,26 +1,95 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Character {
+    private String name;
     private int tier;
     private int type;
+    private int health;
     private int resource;
-    private int[] stats;
-    private String[] moves;
+    private int[] stats;//first element is max health, second is max resource, third is speed, fourth is accuracy die, fifth is damage die, six is dodge die
+    private ArrayList<String> moves;
 
     public Character(int t, int ty){
         tier = t;
         type = ty;
+        stats = new int[6];
+    }
+    public Character(String n, int t, int ty, int h, int r, int[] st, ArrayList<String> mo){ //framework for reading characters from files
+        name = n;
+        tier = t;
+        type = ty;
+        health = h;
+        resource = r;
+        stats = st;
+        moves = mo;
     }
 
-    public void setStats(int[] s) {
-        stats = s;
+    public void setHealth(int h) {
+        health = h;
     }
-    public void setMoves(String[] m) {
+    public void setResource(int r) {
+        resource = r;
+    }
+    //prerequisite: s is an int array with same length as stats
+    public void setStats(int[] s) {
+        for (int i = 0; i < stats.length; i++) {
+            stats[i] = s[i];
+        }
+    }
+    public void setMoves(ArrayList<String> m) {
         moves = m;
     }
 
-    public int[] getStats() {
-        return stats;
+    public String getName() {
+        return name;
     }
-    public String[] getMoves() {
-        return moves;
+    public int getTier() {
+        return tier;
+    }
+    public String getType() {
+        if (type == 1){
+            return "Gunslinger";
+        } else if (type == 2){
+            return "Melee";
+        } else if (type == 3){
+            return "Wizard";
+        } else {
+            return "Shielder";
+        }
+    }
+    public int getHealth() {
+        return health;
+    }
+    public int getResource() {
+        return resource;
+    }
+    public String getStats() {
+        String temp = "Stat Array: ";
+        temp += Arrays.toString(stats);
+        return temp;
+    }
+    public String getMoves() {
+        String temp = "Moveset: ";
+        temp += moves;
+        return temp;
+    }
+
+    public String toString() {
+        return getName() + "\nTier: " + getTier() + "\nType: " + getType() + "\nHealth: " + getHealth() + "\nResource: " + getResource() + "\n" + getStats() + "\n" + getMoves();
+    }
+
+    public void writeToFile() {
+        try {
+            FileWriter myWriter = new FileWriter("Characters.txt");
+            myWriter.write(toString());
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
--- src/FrontDoor.java
+++ src/FrontDoor.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+
 public class FrontDoor {//Misc Test class
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
-            System.out.println(Moves.sh_move());
-        }
+        int[] stats = {1,2,3,4,5,6};
+        ArrayList<String> moves = new ArrayList<String>();
+        moves.add("Test");
+        moves.add("123");
+        moves.add("Guns");
+        Character test = new Character("Test", 4, 1, 5, 6, stats, moves);
+        test.writeToFile();
     }
 }

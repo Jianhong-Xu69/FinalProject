b9e58788b79abb13daaed11cd621dcef8adf783b
BT_4N2_15 <jxu5406@bths.edu>
2023-05-30 12:10:10 -0400

    Patch v0.14
    "Borrowed" UI test code
    Trying to find out how this all works

--- src/FrontDoor.java
+++ src/FrontDoor.java
@@ -1,14 +1,10 @@
+import javax.swing.*;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 
 public class FrontDoor {//Misc Test class
     public static void main(String[] args) {
-        Player test = new Player();
-        test.pullFromFile();
-        Combat tes = new Combat(test);
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Target " + tes.findTarget() + "\n");
-        }
+
     }
 }
--- src/PartyGUI.java
+++ src/PartyGUI.java
@@ -1,10 +1,47 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class PartyGUI {
-    private JButton button1;
-    private JButton button2;
-    private JTextField textField1;
+    public static void main(String[] args) {
+        ImageIcon lolang = new ImageIcon("src/sprites/GSidle.png");//rightclick on image, copy path from source root
+        Border border = BorderFactory.createLineBorder(Color.blue, 4);
 
+        JPanel rpanel = new JPanel();
+        rpanel.setBackground(Color.red);
+        rpanel.setBounds(0,0,100,100);
+
+        JPanel lolangpanel = new JPanel();
+        lolangpanel.setBackground(Color.lightGray);
+        lolangpanel.setBounds(100,100,500,500);
+        lolangpanel.setLayout(null);
+
+        JLabel label = new JLabel();
+        label.setText("Krill");
+        label.setIcon(lolang);
+        label.setForeground(new Color(0, 150, 255));
+        label.setBackground(new Color(255,255,255));
+        label.setOpaque(true);
+        label.setBorder(border);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP); //above, center, below
+//        label.setIconTextGap(10); //distance from text to image
+//        label.setVerticalAlignment(JLabel.BOTTOM);
+//        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setBounds(0,0, 110, 330);//overrides alignments (x,y,wide,height) (needs null
+
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Testing frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(true);
+        frame.setSize(1000,1000);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        lolangpanel.add(label);
+        frame.add(rpanel);
+        frame.add(lolangpanel);
+    }
 }
